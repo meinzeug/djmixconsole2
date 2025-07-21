57760f7efa2b262ba56b0c8bd7fc74539b59e727
meinzeug <dennis.wicht@web.de>
2025-07-21 05:29:20 +0200

    Add error boundary and HMR handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,25 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
+
+if (import.meta.hot) {
+  import.meta.hot.accept();
+  import.meta.hot.dispose(() => root.unmount());
+}
+
